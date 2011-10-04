e0ac57850354f3b6418859998f5aa0091d087caa
Andy Stewart <boss@airbladesoftware.com>
2011-10-04 16:37:03 +0200

    Generator takes optional model name.

--- lib/generators/quo_vadis/install_generator.rb
+++ lib/generators/quo_vadis/install_generator.rb
@@ -8,6 +8,7 @@ module QuoVadis
     extend ActiveRecord::Generators::Migration
 
     source_root File.expand_path('../templates', __FILE__)
+    argument :model_name, :type => :string, :default => 'User'
 
     desc 'Copies an initializer, a locale file, and a migration to your application.'
 
@@ -17,11 +18,11 @@ module QuoVadis
     end
 
     def copy_initializer_file
-      copy_file '../../../../config/initializers/quo_vadis.rb', 'config/initializers/quo_vadis.rb'
+      template 'quo_vadis.rb.erb', 'config/initializers/quo_vadis.rb'
     end
 
     def create_migration_file
-      migration_template 'migration.rb', 'db/migrate/add_authentication_to_users.rb'
+      migration_template 'migration.rb.erb', "db/migrate/add_authentication_to_#{model_name.tableize}.rb"
     end
 
   end
