239fb91694ef9f6f863a5860087250f0aee67392
Andy Stewart <boss@airbladesoftware.com>
2012-02-07 16:43:51 +0100

    Upgrade Capybara.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -79,7 +79,7 @@ def get_cookie(cookie_name)
 end
 
 def cookie_jar
-  Capybara.current_session.driver.current_session.instance_variable_get(:@rack_mock_session).cookie_jar
+  Capybara.current_session.driver.browser.current_session.instance_variable_get(:@rack_mock_session).cookie_jar
 end
 
 def close_browser
