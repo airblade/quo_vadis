629ca6ffe50d8cf4d4bd2f88a1e9b4473ca73eee
Andy Stewart <boss@airbladesoftware.com>
2021-06-28 11:44:27 +0100

    Update install task following 00ff894

--- lib/generators/quo_vadis/install_generator.rb
+++ lib/generators/quo_vadis/install_generator.rb
@@ -1,6 +1,6 @@
 module QuoVadis
   class InstallGenerator < Rails::Generators::Base
-    source_root Pathname.new(__dir__) / '..' / '..' / '..' / 'test' / 'dummy' / 'app' / 'views' / 'quo_vadis'
+    source_root Pathname.new(__dir__) / '..' / '..' / '..' / 'app' / 'views' / 'quo_vadis'
 
     desc "Copy QuoVadis' views into your app."
     def copy_views
